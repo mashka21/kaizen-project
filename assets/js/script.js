f5e90e3cc21db6ee4de278fe9bc9c63acaf4f421
mashka21 <mashquul252@gmail.com>
2022-06-14 16:42:58 +0600

    intership project for kaizen began

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        center:true,
+        nav:false,
+        margin:10,
+        responsiveClass:true,
+        responsive: {
+            0:{
+                item:1,
+            },
+            600:{
+                item:1
+            },
+            1000:{
+                item:3
+            },
+
+        } 
+
+    })
+})
